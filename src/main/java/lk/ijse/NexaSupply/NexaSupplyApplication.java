51fba44f520c9097ffd1f9f018549895a18581a1
InduniPalliyaguru <lakmiindu11@gmail.com>
2026-07-28 17:49:10 +0530

    initialize NexaSupply project with Spring Boot setup and Maven wrapper

---
+++ src/main/java/lk/ijse/NexaSupply/NexaSupplyApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.NexaSupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaSupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexaSupplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/NexaSupply/NexaSupplyApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.NexaSupply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexaSupplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
